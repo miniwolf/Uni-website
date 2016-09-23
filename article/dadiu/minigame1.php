667a71585a6d843d26bd67e8ed55ae72996ddba2
s154902 <s154902@student.dtu.dk>
2016-09-23 11:39:39 +0200

    Fixing issues in references and updating profile page.
    Beginning dadiu minigame 1 production page.

--- article/dadiu/minigame1.php
+++ article/dadiu/minigame1.php
@@ -1,8 +1,8 @@
 <?php
-$root = '../../';
-require_once($root . 'resources/variables.php');
-$title = 'Minigame';
-$desc = '...';
-$links = ["style.css"];
-$content = require_once("miniegame1.html");
-require_once($root . 'master.php');
+
+include("../../parser/MasterParser.php");
+$masterParser = new \parser\MasterParser('../..', 'DADIU Minigame 1');
+$masterParser->setContentFile('minigame1.html');
+$masterParser->setLinks(['style.css']);
+$masterParser->setDesc('First minigame for DADIU');
+require_once('../../master.php');
--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 include("parser/MasterParser.php");
 $masterParser = new \parser\MasterParser(".", 'FrontPage');
 $masterParser->setDesc('Newspage for game related news');
-$masterParser->setContentFile('/index/index.html');
+$masterParser->setContentFile('page.html');
 require_once('master.php');
